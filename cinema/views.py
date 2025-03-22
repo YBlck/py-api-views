566989ed79db2afa611ac0d10a4f251fc46bffbf
Yura Chornyi <chorny.yura@gmail.com>
2025-03-22 04:53:11 +0200

    fix: model movie fix related name, GenreList view fix errors response

--- cinema/views.py
+++ cinema/views.py
@@ -28,6 +28,7 @@ class GenreList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GenreDetail(APIView):
